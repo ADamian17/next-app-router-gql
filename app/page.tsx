c694b0b32e15e8cf9016660966c72224bf37681e
ADamian17 <adonisdesuvikdo@gmail.com>
2023-12-17 22:03:49 -0800

    added gql types

--- app/page.tsx
+++ app/page.tsx
@@ -5,46 +5,46 @@ import { createApolloClient } from "@/libs/apolloClient";
 import MainLayout from "@/layouts/MainLayout";
 
 export default async function Home() {
-  // const client = createApolloClient();
-  // const { data } = await client.query({
-  //   query: gql`
-  //     query Page{
-  //       page(id: "/", idType: URI) {
-  //         title
-  //         id
-  //         template {
-  //           ... on HomeTemplate {
-  //             templateName
-  //             postRelationship {
-  //               postList {
-  //                 ... on Post {
-  //                   id
-  //                   slug
-  //                 }
-  //               }
-  //             }
-  //           }
-  //         }
-  //         content
-  //       }
-  //     }
-  //   `
-  // });
+  const client = createApolloClient();
+  const { data } = await client.query({
+    query: gql`
+      query Page{
+        page(id: "/", idType: URI) {
+          title
+          id
+          template {
+            ... on HomeTemplate {
+              templateName
+              postRelationship {
+                postList {
+                  ... on Post {
+                    id
+                    slug
+                  }
+                }
+              }
+            }
+          }
+          content
+        }
+      }
+    `
+  });
 
-  // const options: HTMLReactParserOptions = {
-  //   replace(node) {
-  //     return node;
-  //   },
-  // };
+  const options: HTMLReactParserOptions = {
+    replace(node) {
+      return node;
+    },
+  };
 
   return (
     <MainLayout>
       Main Page
-      {/* <section className='max-w-screen-xl mx-auto mt-8'>
+      <section className='max-w-screen-xl mx-auto mt-8'>
         <h1 className='font-medium'>{data?.page?.title}</h1>
 
         {parse(data?.page?.content, options)}
-      </section> */}
+      </section>
     </MainLayout>
   )
 }
